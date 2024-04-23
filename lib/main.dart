9d19de16d09c15bad56bf0739f4194eb75f02e66
Jalal <jalal13elzein@gmail.com>
2024-04-23 21:35:34 +0300

    implemented bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:franz/pages/about.dart';
+import 'package:franz/pages/contact.dart';
+import 'package:franz/pages/settings.dart';
+import 'package:franz/pages/transcribe.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Franz'),
     );
   }
 }
@@ -32,12 +35,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int currentPageIndex = 1;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget _buildBody() {
+    switch (currentPageIndex) {
+      case 0:
+        return const SettingsScreen();
+      case 1:
+        return const TranscribeScreen();
+      case 2:
+        return const ContactScreen();
+      case 3:
+        return const AboutScreen();
+      default:
+        return const SizedBox.shrink();
+    }
   }
 
   @override
@@ -47,25 +59,43 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.music_note),
+            icon: Icon(Icons.music_note_outlined),
+            label: 'Transcription',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.mail),
+            icon: Icon(Icons.mail_outline),
+            label: 'Contact',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.info),
+            icon: Icon(Icons.info_outline),
+            label: 'About',
+          ),
+        ],
+      ),
+      body: _buildBody(),
     );
   }
 }
---
+++ lib/pages/transcribe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TranscribeScreen extends StatefulWidget {
+  const TranscribeScreen({super.key});
+
+  @override
+  State<TranscribeScreen> createState() => _TranscribeScreenState();
+}
+
+class _TranscribeScreenState extends State<TranscribeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

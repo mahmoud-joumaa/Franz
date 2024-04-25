6ac37a39391b57e7d5054d9f43dfd762e298e992
ahmad <ahmadhassanhussein2003@gmail.com>
2024-04-25 14:53:20 +0300

    fixed keyboard overflow errors

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContactScreen extends StatefulWidget {
@@ -21,49 +22,53 @@ class _ContactScreenState extends State<ContactScreen> {
           const Align(
             alignment: Alignment.topCenter,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Text("Rating", style: TextStyle(fontSize: 30)),
-              const Spacer(),
-              IconButton(onPressed: (){setState(() {rating = 1;});}, icon: Icon(rating > 0 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
-              IconButton(onPressed: (){setState(() {rating = 2;});}, icon: Icon(rating > 1 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
-              IconButton(onPressed: (){setState(() {rating = 3;});}, icon: Icon(rating > 2 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
-              IconButton(onPressed: (){setState(() {rating = 4;});}, icon: Icon(rating > 3 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
-              IconButton(onPressed: (){setState(() {rating = 5;});}, icon: Icon(rating > 4 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
-            ],
+          Container(
+            margin: const EdgeInsets.only(bottom: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text("Rating", style: TextStyle(fontSize: 30)),
+                const Spacer(),
+                IconButton(onPressed: (){setState(() {rating = 1;});}, icon: Icon(rating > 0 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
+                IconButton(onPressed: (){setState(() {rating = 2;});}, icon: Icon(rating > 1 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
+                IconButton(onPressed: (){setState(() {rating = 3;});}, icon: Icon(rating > 2 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
+                IconButton(onPressed: (){setState(() {rating = 4;});}, icon: Icon(rating > 3 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
+                IconButton(onPressed: (){setState(() {rating = 5;});}, icon: Icon(rating > 4 ? Icons.star : Icons.star_border, color: Colors.orange, size: 30,)),
+              ],
+            ),
           ),
-          Expanded(
-            child: Container(
-              alignment: Alignment.centerLeft,
-              child: DropdownButtonFormField<String>(
-                value: category,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text("Message Type"),
-                ),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    category = newValue!;
-                  });
-                },
-                items: <String>['Report Bug', 'Provide Feedback', 'Suggest Feature']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
+          Container(
+            margin: const EdgeInsets.only(bottom: 15),
+            alignment: Alignment.centerLeft,
+            child: DropdownButtonFormField<String>(
+              value: category,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Message Type"),
               ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  category = newValue!;
+                });
+              },
+              items: <String>['Report Bug', 'Provide Feedback', 'Suggest Feature']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
           ),
 
-          TextField(
-            controller: messageController,
-            maxLines: 13,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              label: Text("Message"),
+          Expanded(
+            child: TextField(
+              controller: messageController,
+              maxLines: 100,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Message"),
+              ),
             ),
           ),
           Row(
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -19,130 +19,135 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb5ay974Ak1bGFIDStEQaYK7qK60bzbbmczDft-ao-Xw&s'),
-              ),
-              SizedBox(width: 30,),
-              Column(
-                children: [
-                  Text("Welcome,", style: TextStyle(fontSize: 20),),
-                  Text("Username"),
-                ],
-              )
-            ],
-          ),
-          const SizedBox(height: 20,),
-          Card(
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child:
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: DropdownButtonFormField<String>(
-                            value: language,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              label: Text("Select Language"),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb5ay974Ak1bGFIDStEQaYK7qK60bzbbmczDft-ao-Xw&s'),
+                ),
+                SizedBox(width: 30,),
+                Column(
+                  children: [
+                    Text("Welcome,", style: TextStyle(fontSize: 20),),
+                    Text("Username"),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                height: 270,
+                child:
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.centerLeft,
+                            child: DropdownButtonFormField<String>(
+                              value: language,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                label: Text("Select Language"),
+                              ),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  language = newValue!;
+                                });
+                              },
+                              items: <String>['English', 'Arabic', 'Spanish']
+                                  .map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
                             ),
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                language = newValue!;
-                              });
-                            },
-                            items: <String>['English', 'Arabic', 'Spanish']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
                           ),
                         ),
-                      ),
-                      const SizedBox(width: 20,),
-                      const Icon(Icons.wb_sunny, color: Colors.orange,),
-                      Switch(
-                        value: isDarkMode,
-                        onChanged: (value) {
-                          setState(() {
-                            isDarkMode = value;
-                          });
-                        },
-                      ),
-                      const Icon(Icons.nights_stay, color: Colors.black,),
-                    ],
-                  ),
-                  const SizedBox(height: 20,),
-                  Card(
-                    color: Colors.grey[200],
-                    child: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: usernameController,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              label: Text("Change Username"),
-                            ),
-                          ),
-                          const SizedBox(height: 30,),
-                          Row(
+                        const SizedBox(width: 20,),
+                        const Icon(Icons.wb_sunny, color: Colors.orange,),
+                        Switch(
+                          value: isDarkMode,
+                          onChanged: (value) {
+                            setState(() {
+                              isDarkMode = value;
+                            });
+                          },
+                        ),
+                        const Icon(Icons.nights_stay, color: Colors.black,),
+                      ],
+                    ),
+                    const SizedBox(height: 20,),
+                    Expanded(
+                      child: Card(
+                        color: Colors.grey[200],
+                        child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
                             children: [
-                              TextButton(
-                                  onPressed: () => usernameController.text = "USERNAME", child: const Text("Cancel")),
-                              const Spacer(),
-                              TextButton(
-                                  onPressed: confirmUsername, child: const Text("Confirm")),
+                              TextField(
+                                controller: usernameController,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  label: Text("Change Username"),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  TextButton(
+                                      onPressed: () => usernameController.text = "USERNAME", child: const Text("Cancel")),
+                                  const Spacer(),
+                                  TextButton(
+                                      onPressed: confirmUsername, child: const Text("Confirm")),
+                                ],
+                              )
                             ],
-                          )
-                        ],
+                          ),
+                        ),
+                      
                       ),
                     ),
-
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Card(
-              child: Column(
-                children: [
-                  Container(
-                    alignment: Alignment.centerLeft,
-                      child: const Row(
-                        children: [
-                          Icon(Icons.warning, size: 40, color: Colors.red,),
-                          Text("DANGER ZONE", style: TextStyle(color: Colors.red),),
-                        ],
-                      )
-                  ),
-                  Row(
-                    children: [
-                      TextButton(onPressed: () => displayWarning(disableAccount),
-                          child: const Text("Disable Account")),
-                      const Spacer(),
-                      TextButton(onPressed: () => displayWarning(deleteAccount),
-                          child: const Text("Delete Account")),
-                    ],
-                  ),
-                ],
+            Card(
+              child: Container(
+                height: 100,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                        child: const Row(
+                          children: [
+                            Icon(Icons.warning, size: 40, color: Colors.red,),
+                            Text("DANGER ZONE", style: TextStyle(color: Colors.red),),
+                          ],
+                        )
+                    ),
+                    Row(
+                      children: [
+                        TextButton(onPressed: () => displayWarning(disableAccount),
+                            child: const Text("Disable Account")),
+                        const Spacer(),
+                        TextButton(onPressed: () => displayWarning(deleteAccount),
+                            child: const Text("Delete Account")),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
